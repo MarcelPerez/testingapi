52df32941720d3497e724c1bc7d1280d76d011d5
Marcel <mar>
2023-02-20 10:25:32 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount(){
+    fetch('http://10.147.18.174:7111/api/Cars')    
+    .then(response => response.json())
+    .then(response=>{console.log(response)});
+  }
+
+  render() {
+    return <div><iframe src="http://10.147.18.139:3000/d-solo/D1oAjtJVk/new-dashboard?orgId=1&from=1676464004786&to=1676485604786&panelId=2" title=""
+    width="1000" height="750"></iframe></div>;
+  }
 }
 
 export default App;
